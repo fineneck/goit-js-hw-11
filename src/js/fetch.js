83c4630d5e41ee65246a1a25f21ad8d00618f5f4
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-30 11:20:56 +0300

    create fetch

---
+++ src/js/fetch.js
@@ -0,0 +1,14 @@
+// const options = {
+//   headers: {
+//     Authorization: '34878680-c3ff6e2c0cd5a850aab6b1444',
+//   },
+// };
+
+const KEY = '34878680-c3ff6e2c0cd5a850aab6b1444';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const url = ''+ BASE_URL +'?key='+ KEY +'&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1';
+
+fetch(url)
+  .then(response => response.json())
+  .then(console.log);
