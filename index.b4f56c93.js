6e0be3bc85a87182aaf0455393f18ec86e050f20
fineneck <111312525+fineneck@users.noreply.github.com>
2023-03-31 08:00:18 +0000

    Deploying to gh-pages from @ fineneck/goit-js-hw-11@852a161c4f609a2bcad4b55870a0a09849d59594 🚀

--- index.b4f56c93.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/?key=34878680-c3ff6e2c0cd5a850aab6b1444&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1").then((e=>e.json())).then(console.log);
-//# sourceMappingURL=index.b4f56c93.js.map
--- index.ce32e029.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/?key=34878680-c3ff6e2c0cd5a850aab6b1444&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1").then((function(e){return e.json()})).then(console.log);
-//# sourceMappingURL=index.ce32e029.js.map
