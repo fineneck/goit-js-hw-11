852a161c4f609a2bcad4b55870a0a09849d59594
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-31 10:59:40 +0300

    pagination fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-import fetch from './js/fetch';
+// import fetch from './js/fetch';
+import ImageApiService from './js/image-service';
+import imageTpl from './templates/markupHits.hbs';
+
+
+const imageApiService = new ImageApiService();
+
+const refs = {
+  search: document.querySelector('.search-form'),
+  input: document.querySelector('.input'),
+  gallery: document.querySelector('.gallery'),
+  btnLoad: document.querySelector('.load-more')
+};
+
+refs.search.addEventListener('submit', onSearch);
+refs.btnLoad.addEventListener('click', onLoad);
+
+
+function onSearch(e) {
+  e.preventDefault();
+
+  imageApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+  imageApiService.resetPage();
+  imageApiService.fetchImages().then(appendImageMarkup);
+  
+};
+
+function onLoad() {
+  imageApiService.fetchImages().then(appendImageMarkup);
+};
+
+function appendImageMarkup(hits) {
+  refs.gallery.insertAdjacentHTML('beforeend', imageTpl(hits));
+}
---
+++ src/js/image-service.js
@@ -0,0 +1,40 @@
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+
+  const API_KEY = '34878680-c3ff6e2c0cd5a850aab6b1444';
+  const BASE_URL = 'https://pixabay.com/api/';
+  
+  const url = ''+ BASE_URL +'?key='+ API_KEY +'&q='+ this.searchQuery +'&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&'+ this.page +'';
+  
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      this.incrementPage();
+      console.log(data);
+      return data.hits;
+     
+    });
+    
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+  
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
