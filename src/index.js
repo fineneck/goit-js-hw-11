dc1f7b6d0a9caf3a3286dcfd99f19092fa8a85d0
OleksiiMikhieiev <babybong0993@gmail.com>
2023-04-02 02:59:02 +0300

    made load btn

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
 // import fetch from './js/fetch';
 import ImageApiService from './js/image-service';
 import imageTpl from './templates/markupHits.hbs';
+import LoadMoreBtn from './js/load-more-btn';
 
 
 const imageApiService = new ImageApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
 
 const refs = {
   search: document.querySelector('.search-form'),
   input: document.querySelector('.input'),
   gallery: document.querySelector('.gallery'),
-  btnLoad: document.querySelector('.load-more')
 };
 
 refs.search.addEventListener('submit', onSearch);
-refs.btnLoad.addEventListener('click', onLoad);
+loadMoreBtn.refs.button.addEventListener('click', fetchImages);
 
 
 function onSearch(e) {
   e.preventDefault();
 
-  // clearImageGallery();
   imageApiService.searchQuery = e.currentTarget.elements.searchQuery.value.trim();
 
   if (imageApiService.searchQuery === '') {
     return alert('Введи что-то');
   };
 
+  loadMoreBtn.show();
   imageApiService.resetPage();
-  imageApiService.fetchImages().then(hits => {
-    clearImageGallery();
-    appendImageMarkup(hits);
-    
-  });
-  
+  clearImageGallery();
+  fetchImages();  
 };
 
-function onLoad() {
-  imageApiService.fetchImages().then(appendImageMarkup);
+function fetchImages() {
+  loadMoreBtn.disable();
+  imageApiService.fetchImages().then(hits => {  
+    appendImageMarkup(hits);
+    loadMoreBtn.enable();
+  });
 };
 
 function appendImageMarkup(hits) {
--- src/js/load-more-btn.js
+++ src/js/load-more-btn.js
@@ -13,4 +13,24 @@ export default class LoadMoreBtn {
 
     return refs;
   }
+
+  enable() {
+    this.refs.button.disabled = false;
+    this.refs.label.textContent = 'Load more';
+    this.refs.spinner.classList.add('is-hidden');
+  }
+
+  disable() {
+    this.refs.button.disabled = true;
+    this.refs.label.textContent = 'Loading...';
+    this.refs.spinner.classList.remove('is-hidden');
+  }
+
+  show() {
+    this.refs.button.classList.remove('is-hidden');
+  }
+
+  hide() {
+    this.refs.button.classList.add('is-hidden');
+  }
 }
